7463399e043bafcfabcca1562446532588ce04e0
Isa-chacon <137378822+Isa-chacon@users.noreply.github.com>
2024-06-13 14:35:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('calculator-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const number = parseFloat(document.getElementById('number-input').value);
+    if (isNaN(number)) {
+        alert('Por favor, ingresa un número válido.');
+        return;
+    }
+
+    const multiplyResult = (number * 32000).toLocaleString();
+    const divideResult = (number / 150).toLocaleString();
+
+    document.getElementById('multiply-result').textContent = `Costo aproximado del sistema $ ${multiplyResult}`;
+    document.getElementById('divide-result').textContent = `Kilovatios pico ${divideResult}`;
+});
